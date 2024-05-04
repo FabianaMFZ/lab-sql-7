212d46a54bac5e8947c7653f89a9990f0301649b
FabianaMFZ <fabianamfz@gmail.com>
2024-05-04 16:04:29 +0100

    'lab'

--- lab_sql_5.sql
+++ lab_sql_5.sql
@@ -1,5 +1,3 @@
--- Instructions
-
 -- In the table actor, which are the actors whose last names are not repeated? 
 	-- For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. 
 	-- These three actors have the same last name. So we do not want to include this last name in our output. 
@@ -26,22 +24,25 @@ order by last_name;
 select staff_id, count(rental_id)
 from sakila.rental
 group by staff_id
-order by staff_id asc;
+order by count(rental_id);
 
 -- Using the film table, find out how many films were released each year.
 select release_year, count(title)
 from sakila.film
-group by release_year;
+group by release_year
+order by count(title);
 
 -- Using the film table, find out for each rating how many films were there.
 select rating, count(title)
 from sakila.film
-group by rating;
+group by rating
+order by count(title);
 
 -- What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
 select rating, round(avg(length), 2) as avg_length
 from sakila.film
-group by rating;
+group by rating
+order by avg_length;
 
 -- Which kind of movies (rating) have a mean duration of more than two hours?
 select rating, round(avg(length), 2) as avg_length
